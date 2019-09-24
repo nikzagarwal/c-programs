43c68b80c222cead1fdbabfc54b92817e15ad9fc
Nikhil Agarwal <nikhilagarwalassisi@gmail.com>
2019-09-24 21:47:59 +0530

    prg to avg 2 no.

---
+++ 2-Avg_2_no.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void main()
+{
+	int a,b,avg;
+	cout<<"Enter a and b"<<endl;
+	cin>>a>>b;
+	avg=(a+b)/2;
+	cout<<"Average ="<<avg;
+	getch();
+}
